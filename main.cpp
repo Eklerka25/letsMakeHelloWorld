a83e5abbae2c5458c130d4066d9e9df7154aa8ef
Eklerka25 <134164161+Eklerka25@users.noreply.github.com>
2025-01-22 18:19:10 +0100

    First version

---
+++ main.cpp
@@ -0,0 +1,20 @@
+// LETS MAKE HELLO WORLD IN C++
+
+#include <iostream>
+#include <cstdint>
+#include <stdint.h>
+#include <stdio.h>
+#include <cstdlib>
+#include <stdlib.h>
+
+#ifndef YUTGIFUGYIGYUIFGYUIDSAYUTIUTYDYUGSFGHUDTUHAVFTUYDTUYFUTYTYFUYTS
+#ifndef UYFURUIGYWERBIGFHUIGADCYGFDSTCISDGYGTUIFYTAUWRTFGDSGFUTYDYTGFYt
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    // Here put rest of the code
+}
+
+#endif
+#endif
