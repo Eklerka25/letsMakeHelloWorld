01b1d956393bf599ab84248be1073ee89bcef36d
Eklerka25 <134164161+Eklerka25@users.noreply.github.com>
2025-01-22 18:39:16 +0100

    Version 2

--- main.cpp
+++ main.cpp
@@ -1,19 +1,66 @@
 // LETS MAKE HELLO WORLD IN C++
 
+#define ABC <<
+#define CBA >>
+
 #include <iostream>
 #include <cstdint>
 #include <stdint.h>
 #include <stdio.h>
 #include <cstdlib>
 #include <stdlib.h>
+#include <string>
+#include <conio.h>
+
+#pragma twice
 
 #ifndef YUTGIFUGYIGYUIFGYUIDSAYUTIUTYDYUGSFGHUDTUHAVFTUYDTUYFUTYTYFUYTS
 #ifndef UYFURUIGYWERBIGFHUIGADCYGFDSTCISDGYGTUIFYTAUWRTFGDSGFUTYDYTGFYt
 
 using namespace std;
 
+int ReturnOnlyNumberTwenty() {
+    // this function will return number twenty :D
+    return 35;
+}
+
+#define DISPLAY_TEXT_ONTO_THE_SCREEN_PLEASE cout
+
+int FunctionThatDoesntReturnIntegerTwenty() {
+    // this function won't return number twenty
+    return 20;
+}
+
+
+typedef char* not_sigma;
+typedef std::string sigma;
+
+#define SET_VALUE_TO =
+#define CLOSED_BRACKET }
+#define SEMICOLON ;
+
 int main(int argc, char* argv[]) {
-    // Here put rest of the code
+    int a = 10;
+    int b = ReturnOnlyNumberTwenty() + 3;
+    int c = b;
+    int d = c;
+
+    sigma x SET_VALUE_TO "ohio" SEMICOLON
+
+    if (
+        2
+        // HELLO
+        // UIH
+        ==
+        // OK
+        2
+    ){int e = 1;}
+
+    // this is so hard
+
+    char cplusplusismyfavouritelanguage = getch();
+
+    DISPLAY_TEXT_ONTO_THE_SCREEN_PLEASE ABC "You've pressed: some key on the keyboard" SEMICOLON
 }
 
 #endif
